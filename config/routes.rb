11bab28a748821a81f7de6a2ea5b1e7d7a4da069
David Osuna Azcona Dominguez <david.oad@gmail.com>
2020-05-21 18:58:46 -0600

    forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'activities/create'
-  get 'activities/update'
-  root to: 'itineraries#home'
   resources :itineraries do
     resources :activities, only: :create
   end
