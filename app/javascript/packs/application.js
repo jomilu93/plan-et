c6018106e33e54be13264fdb9371013b598b23d8
Lou Malta <lou@loumalta.com>
2020-05-28 20:04:23 -0500

    index improved

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,9 @@ require("@rails/activestorage").start();
 require("channels");
 
 
-//require date_wrappe;
+
+require date_wrapper
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -33,3 +35,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+
