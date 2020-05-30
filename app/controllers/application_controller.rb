081bf52c6d48ce66e7a1b9b96119f773751e26f2
Luis Cesar <lutzcc1@gmail.com>
2020-05-30 14:27:45 -0500

    setup pundit for existing actions of trip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  include Pundit
+
+  # Pundit: white-list approach.
+  after_action :verify_authorized, except: [:index, :home], unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: [:index, :home], unless: :skip_pundit?
+
+
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
   protected
 
   def configure_permitted_parameters
