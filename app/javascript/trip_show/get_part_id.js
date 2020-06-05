8aa7ceab457347ec083ebfab218cc6b99beae760
Luis Cesar <lutzcc1@gmail.com>
2020-06-05 08:55:29 -0500

    modify submit URL with correct part ID

--- app/javascript/trip_show/get_part_id.js
+++ app/javascript/trip_show/get_part_id.js
@@ -1,9 +1,12 @@
-let part_id
-document.querySelectorAll('.plus-sign').forEach(item => {
-  console.log(item.attr('.data-part-id'));
-  item.addEventListener('click', event => {
-    console.log(document.querySelector('.activity_form').action);
-    document.querySelector('.activity_form').action = `/parts/${item}/activities`
-  })
-})
 
+let changeUrl = (id) => {
+  console.log(id);
+  document.querySelector('.activity_form').action = `/parts/${id}/activities`;
+}
+
+document.querySelectorAll('.plus-sign').forEach(item => {
+  console.log(item.getAttribute("data-part-id"));
+  item.addEventListener('click', () => {
+    changeUrl(item.getAttribute("data-part-id"));
+  });
+})
