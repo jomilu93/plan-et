7878e7667057cb02705772c892ceee732a49c80e
David Osuna Azcona Dominguez <david.oad@gmail.com>
2020-06-04 21:58:28 -0600

    almost there 2.0

--- app/javascript/trip_show/get_part_id.js
+++ app/javascript/trip_show/get_part_id.js
@@ -1,6 +1,6 @@
 let part_id
 document.querySelectorAll('.plus-sign').forEach(item => {
-  console.log(item.('.data-part-id'));
+  console.log(item.attr('.data-part-id'));
   item.addEventListener('click', event => {
     console.log(document.querySelector('.activity_form').action);
     document.querySelector('.activity_form').action = `/parts/${item}/activities`
