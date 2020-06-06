3a552a3c9e7a737e40f2f428f310cbcd8217ab8c
Luis Cesar <lutzcc1@gmail.com>
2020-06-05 23:43:30 -0500

    minor details

--- app/javascript/trip_show/get_part_id.js
+++ app/javascript/trip_show/get_part_id.js
@@ -1,9 +1,11 @@
+// change the url to add the part ID the activity belongs to
 let changeUrl = (id) => {
   document.querySelectorAll('.activity_form').forEach(form => {
     form.action = `/parts/${id}/activities`;
   });
 }
 
+// retireve part ID when clicking a '+' sign
 document.querySelectorAll('.plus-sign').forEach(item => {
   item.addEventListener('click', () => {
     changeUrl(item.getAttribute("data-part-id"));
@@ -11,7 +13,7 @@ document.querySelectorAll('.plus-sign').forEach(item => {
 });
 
 
-
+// hide modal when submitting form
 document.querySelectorAll('.submitActivity').forEach(btn => {
   btn.addEventListener('click', () => {
     $('#activityModal').modal('hide');
